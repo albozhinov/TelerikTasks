fa55d3065c5be900ac8604d28f60d17c1e26752c
Bozhinov <a.bozhinov90@gmail.com>
2018-07-09 16:15:32 +0300

    Add exercise Strings

---
+++ Strings/ReverseStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine().ToCharArray();
+            Array.Reverse(inputLine);
+            Console.WriteLine(string.Join("", inputLine));
+        }
+    }
+}
