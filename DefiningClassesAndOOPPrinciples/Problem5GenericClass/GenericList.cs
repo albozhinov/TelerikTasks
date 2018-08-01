7c06af0ee0903681f41ec991d461ad4788531861
a.bozhinov90@gmail.com <a.bozhinov90@gmail.com>
2018-08-01 10:07:53 +0300

    Added conditional tasks

--- DefiningClassesAndOOPPrinciples/Problem5GenericClass/GenericList.cs
+++ DefiningClassesAndOOPPrinciples/Problem5GenericClass/GenericList.cs
@@ -47,7 +47,29 @@ namespace Problem5GenericClass
 
         public int Count { get => this.count; set => this.count = value; }
 
+        public T this[int indexer]
+        {
+            get
+            {
+                Validation(indexer);
+                return this.myList[indexer];    
+            }
+            set
+            {
+                Validation(indexer);
+                this.myList[indexer] = value;
+            }
+        }
+
         // Methods
+        public void Validation(int index)
+        {
+            if (index < 0 || this.Count < index)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
         public void DoubleSize()
         {
             int newSize = this.myList.Length * 2;
@@ -72,7 +94,95 @@ namespace Problem5GenericClass
             this.Count++;
         }
 
+        public void Remove(int index)
+        {
+            Validation(index);
+            var newList = new T[this.Count - 1];
+            int i = 0;
+            int k = 0;
 
+            while (k < this.Count - 1)
+            {
+                if (index == i)
+                {
+                    i++;
+                }
+                newList[k] = this.myList[i];
+                i++;
+                k++;
+            }
 
+            this.Count--;
+            this.myList = newList;
+        }
+
+        public void Insert(int index, T element)
+        {
+            Validation(index);
+
+            if (this.Count == this.Capacity)
+            {
+                DoubleSize();
+            }
+
+            int k = 0;
+            int i = 0;
+            var newList = new T[this.Count + 1];
+
+            while (k <= this.Count)
+            {
+                if (k == index)
+                {
+                    newList[k] = element;                  
+                    k++;
+                    if (k > this.Count + 1)
+                        break;
+                }
+
+                newList[k] = this.myList[i];
+                i++;
+                k++;
+            }
+
+            this.myList = newList;
+            this.Count++;
+        }
+
+        public void Clear()
+        {
+            this.Count = 0;
+            this.Capacity = 4;
+
+            this.myList = new T[Count];
+        }
+
+        public int IndexOf(T value)
+        {
+            int index = 0;
+            for (int i = 0; i < myList.Length; i++)
+            {
+                if (myList[i].Equals(value))
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (i == Count - 1)
+                {
+                    sb.Append(myList[i]);
+                    continue;
+                }
+                sb.Append(myList[i] + ", ");                    
+            }
+            return sb.ToString();
+        }
     }
 }
--- DefiningClassesAndOOPPrinciples/Problem5GenericClass/Program.cs
+++ DefiningClassesAndOOPPrinciples/Problem5GenericClass/Program.cs
@@ -5,12 +5,36 @@ namespace Problem5GenericClass
     class Program
     {
         static void Main(string[] args)
-        {
-            
+        {            
 
+            GenericList<int> myList = new GenericList<int>();
+            Console.WriteLine("--------GenericList--------");
+            Console.WriteLine("Capacity: {0} , Count: {1} ", myList.Capacity, myList.Count);
 
+            // Test Count,Capacity and Add
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Adding 20 elements...\n");
+            for (int i = 0; i < 20; i++)
+            {
+                myList.Add(i + 5);
+            }
 
+            Console.WriteLine("--------GenericList--------");
+            Console.WriteLine("Capacity: {0} , Count: {1} ", myList.Capacity, myList.Count);
+            Console.WriteLine(myList);
 
+            //Test  RemoveAt ,InsertAt and FIndIndex
+            myList.Insert(5, 1111);
+            myList.Insert(6, 2222);
+            myList.Insert(7, 3333);
+
+            myList.Remove(10);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("--------GenericList--------");
+            Console.WriteLine("Capacity: {0} , Count: {1} ", myList.Capacity, myList.Count);
+            Console.WriteLine(myList);
+            Console.WriteLine("\nThe element {0} is at {1} position.", 3333, myList.IndexOf(3333));
         }
     }
 }
