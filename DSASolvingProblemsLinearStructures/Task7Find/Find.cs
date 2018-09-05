17978fed897dd09f93999a529667ede97afd7e81
a.bozhinov90@gmail.com <a.bozhinov90@gmail.com>
2018-09-06 02:21:36 +0300

    Added Task7 and Task8

---
+++ DSASolvingProblemsLinearStructures/Task7Find/Find.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task7Find
+{
+    class Find
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new List<int>() { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+
+            FindOccur(sequence);          
+        }
+        public static void FindOccur(List<int> sequence)
+        {            
+            sequence.GroupBy(e => e)
+                    .OrderBy(e => e.Key)
+                    .ToList()
+                    .ForEach(e =>
+                    Console.WriteLine($"{e.Key} -> {e.Count()} times"));
+        }
+    }
+}
---
+++ DSASolvingProblemsLinearStructures/Task8FindTheMajorant/Majorant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task8FindTheMajorant
+{
+    class Majorant
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new List<int>() { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
+
+            FindMajorant(sequence);
+        }
+        public static void FindMajorant(List<int> sequence)
+        {
+            var result = sequence.GroupBy(e => e)
+                                 .Where(e => e.Count() == (sequence.Count / 2) + 1)
+                                 .ToList();
+
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No majorant found!");
+            }
+            else
+            {
+                result.ForEach(e => Console.WriteLine($"Majorant: {e.Key}"));
+            }
+        }
+    }
+}
